278a64cdfba57f7cc60d2aef86c2e775cd2bde3a
fightforfreedom2k3 <nhhoang15122003@gmail.com>
2025-09-30 20:19:56 +0700

    update react

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8000/api', // Thay đổi nếu cần
+  baseURL: 'https://vehicle-manage.vercel.app/api', // Thay đổi nếu cần
   headers: {
     'Content-Type': 'application/json',
   },
